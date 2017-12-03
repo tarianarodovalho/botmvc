f0cf8761aaa81f53c898010ef2bfb2f058a97c72
tarianarodovalho <tariana.rodovalho@gmail.com>
2017-12-03 14:22:18 -0200

    Remove unnecessary folder

--- BotMVC/src/main/java/ControllerSearch.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-import com.pengrad.telegrambot.model.Update;
-
-public interface ControllerSearch {
-
-	public void search(Update update) throws IOException;
-
-}
--- BotMVC/src/main/java/Observer.java
+++
@@ -1,6 +0,0 @@
-
-public interface Observer {
-
-	public void update(long chatId, String searchData);
-
-}
