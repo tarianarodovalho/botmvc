4f6c56695335e05723c3fc315579b042e7bbba22
tarianarodovalho <tariana.rodovalho@gmail.com>
2017-12-03 13:48:18 -0200

    refine botmvc implementation

---
+++ BotMVC/src/main/java/Lyrics.java
@@ -0,0 +1,32 @@
+
+public class Lyrics {
+
+	private Artist artist;
+	private Song song;
+	private String text;
+
+	public Artist getArtist() {
+		return artist;
+	}
+
+	public void setArtist(Artist artist) {
+		this.artist = artist;
+	}
+
+	public Song getSong() {
+		return song;
+	}
+
+	public void setSong(Song song) {
+		this.song = song;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
---
+++ BotMVC/src/main/java/Song.java
@@ -0,0 +1,18 @@
+
+public class Song {
+	
+	private String name;
+	
+	
+	public Song(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
